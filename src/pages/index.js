dcbf7dda4bd02311898ca8b470629d26f79c79d7
Tim Chege <mail@timchege.com>
2020-10-11 20:45:11 +0300

    4th commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,6 +46,47 @@ const IndexPage = () => (
         />
       </div>
     </div>
+    <div className="ProductLanguage">
+      <h2>Product language that we bash</h2>
+      <div className="ProductLanguageText">
+        <p>"This is the idea,can't you just design it? Research will take time..."</p>
+        <p>"Add this paragragh, the more the content the more its understandable..."</p>
+        <p>"So you're a product designer? Please make this pop. Give it some joos..."</p>
+        <p>"I know what the user wants, no need for testing, just launch the app..."</p>
+        <p>"Just follow the manager's instruction, your design process seems like you're over complicating the matter..."</p>
+        <p>"Let's do freemium, we'll figure out the financials later..."</p>
+        <p>"OMG just thought of something really cool. Add this feature, I promise its the next big thing..."</p>
+        <p>"Trust me, its a simple idea.. just uber but for cocktails..."</p>
+        <p>"Trust me, its a simple idea.. just uber but for cocktails..."</p>
+      </div>
+    </div>
+    <div className="AreYouInNeed">
+      <h2>Are you a company & you need...</h2>
+      <div className="AreYouInNeedText">
+        <p>A product re-thought using UX principles?</p>
+        <p>A UX masterclass for your team maybe?</p>
+      </div>
+      <Link to="/contact-us"><button>Let UX Kitchen save your day</button></Link>
+    </div>
+    <div className="Partners">
+      <h3 className="PartnerGroup">Partners:</h3>
+      <Link className="PartnerGroup" to="https://medium.com/@uxkitchenke">
+        <img src={require('../images/at-logo.png')} />
+        <p>Africa's Talking</p>
+      </Link>
+      <Link className="PartnerGroup" to="https://join.slack.com/t/ux-kitchen/shared_invite/zt-eqq3hd4x-EN0MCKTdpc_PShx~aJUilQ">
+        <img src={require('../images/akiliplus-logo.png')} />
+        <p>Akiliplus</p>
+      </Link>
+      <Link className="PartnerGroup" to="https://twitter.com/uxkitchenke">
+        <img src={require('../images/ihub-logo.png')} />
+        <p>iHub Nairobi</p>
+        </Link>
+      <Link className="PartnerGroup" to="https://twitter.com/uxkitchenke">
+        <img src={require('../images/figma-logo.png')} />
+        <p>Figma Nairobi</p>
+      </Link>
+    </div>
   </Layout>
 )
 
