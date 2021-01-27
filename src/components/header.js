ae24cc500b67a91bb727d57566ef237e5e4ffd4d
FieldPro Design <timchege3@gmail.com>
2021-01-27 18:48:12 +0300

    37th commit

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import styled from "styled-components"
 
-import MobileNavMenu from "./MobileNavMenu"
+import MobileNavMenu from "./MobileNavMenu.js"
 import { breakpoints } from "../utils/breakpoints"
 
 import "./header.css"
